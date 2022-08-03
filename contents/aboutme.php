c52e871158bdb92e4a4d1e2747beb72ab31fb2d0
atsam10 <91466610+atsam10@users.noreply.github.com>
2022-08-03 22:43:06 +1000

    about me and home page on progress

--- contents/aboutme.php
+++ contents/aboutme.php
@@ -1,13 +1,29 @@
 <div class="container aboutme p-3">
 
-<div class=" typewitter"><div class="color-lightgreen">01</div> About me </div>     <div class="vl"></div> <br>
-<div class=" pb-2" > <span id="myname">Aashish Gurung</span> <br>
-<span class="" id="intro">I build things for the web.<br></span>
-</div>
+    <div class=" color-white typewitter"><span class="color-lightgreen">01.</span> About me </div> <br>
+   `
 
+    <div class="d-flex">
+        <div class="p-2 w-100">
 
-<div class="pt-4" id="description">
-I'm a <span class="color-lightgreen">web developer</span> based in Sydney I spend most of my day, experimenting with HTML, CSS, and JavaScript (and its endless list of frameworks). I enjoy coding and the challenge of learning something new every day.<br>
-</div>
+            <div class="pt-4" id="abt_description">
+                <p>
+                    Hello! My name is Aashish Gurung and I love Coding, for me, it is a way to bring ideas to life and I extensively use JavaScript to create beautiful web applications.
+                </p>
 
-</div>
+                After receiving my bachelor's degree in information technology with a major in web and mobile application development from Victoria University, I was able to obtain an internship as a software developer at 909IT solutions.
+                <p>
+                    I've also developed various internal applications, such as an online bar manual, an online roster for The Spice Room in Sydney, and a website for 2BRIDGES in Neutral Bay, NSW.
+                </p>
+                My interest and love for programming have grown in recent years, and the internship has convinced me that Web development is my genuine calling.
+                <p>`
+                    Here are a few technologies I have been working with recently:<br>
+                </p>
+
+            </div>
+        </div>
+        <div class="p-2 flex-shrink-1">
+
+          <img src="images/profile.jpg" alt="profile pic" height="396px" width="265px"> 
+        </div>
+    </div>
