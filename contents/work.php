656738162c218073d33ccfc02017a0272c67abb2
atsam10 <91466610+atsam10@users.noreply.github.com>
2022-08-09 23:03:07 +1000

    work page updating

--- contents/work.php
+++ contents/work.php
@@ -1,20 +1,40 @@
-<div class="container work p-4">
-<a  id="exper"></a>
+<div class="container  m-5 px-4">
+    <div class="container  p-4">
+        <a id="exper"></a>
+
+        <div class="container  work">
+            <div class=" title color-white typewitter"><span class="color-lightgreen">02.</span>Where I've worked </div>
+            <div class="hl">
+            </div> <br>
+            <div class="d-flex">
 
 
-    <div class=" title color-white typewitter"><span class="color-lightgreen">02.</span>Where I've worked </div>
-    <div class="hl">
-    </div> <br>
-    <div class="d-flex">
+                <div class="p-2 w-100">
+
+                    <div class="pt-1 " id="work_description">
 
 
-        <div class="p-2 w-100">
-
-            <div class="pt-1 " id="abt_description">
+                        
 
 
+                    </div>
+                </div>
 
             </div>
+
         </div>
 
+
+
+
+
+
+
+
+
+
+
+
     </div>
+
+</div>
--- js/script.js
+++ js/script.js
@@ -1,15 +1,64 @@
-
 // for scroll effect
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
-
   } else {
     document.getElementById("navbar").style.top = "-50px";
-   
   }
   prevScrollpos = currentScrollPos;
-} 
+};
 
+
+function Get_info(work_place){
+  var   wp = document.getElementById("work_place");
+  var   wr = document.getElementById("work_role");
+  var   wpe = document.getElementById("work_period");
+  var   wd = document.getElementById("work_description");
+
+  if(work_place = "909IT"){
+      wr.innerHTML = "Software Developer (Intern)";
+      wpe.innerHTML = "Feb 2022 - Present (6 months +)"
+
+  }
+
+
+}
+
+//________________________________________
+//test for adding work a
+// idk but sth like this garney ho
+
+
+function myF(){
+  var x = document.getElementById("work_description");
+  x.innerHTML = ` <ul> 
+     <li>HTML</li>
+     <li>Cascading Style Sheets (CSS)</li>
+     <li>PHP </li>
+     <li>PhpMyAdmin</li>
+     <li>JavaScript</li>
+     <li>JavaScript frameworks </li>
+     <li>Java</li>
+ </ul>;`
+}
+
+
+// function myFunction() {
+//   var x = document.getElementById("myDIV");
+//   if (x.innerHTML === "Hello") {
+//     x.innerHTML = ` <ul> 
+//     <li>HTML</li>
+//     <li>Cascading Style Sheets (CSS)</li>
+//     <li>PHP </li>
+//     <li>PhpMyAdmin</li>
+//     <li>JavaScript</li>
+//     <li>JavaScript frameworks </li>
+//     <li>Java</li>
+// </ul>;`
+//   } else {
+//     x.innerHTML = "Hello";
+//   }
+// }
+//_____________________________________
